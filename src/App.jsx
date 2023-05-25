27d96fb02071cf5e4e2d94c1b5ec56e2137a8274
Enric <enricmedia83@gmail.com>
2023-05-25 17:32:36 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import ResponsiveAppBar from './components/RespnsiveAppBar'
+import KeepMountedModal from './components/LoginModal'
+import LoginModal from './components/LoginModal'
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <ResponsiveAppBar />
+      </div>
+    </>
+  )
+}
+
+export default App
